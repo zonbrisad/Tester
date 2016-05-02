a10d52ba901056c6646bd4c2462fc626e0f3b006
Peter Malmberg <peter.malmberg@gmail.com>
2016-05-02 18:55:30 +0200

    Initial commit.

---
+++ c++/BoostTest/main.cpp
@@ -0,0 +1,27 @@
+/* 
+ * File:   main.cpp
+ * Author: pmg
+ *
+ * Created on den 28 juli 2012, 19:23
+ */
+
+#include <stdio.h>
+#include <cstdlib>
+
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+  int i;
+  printf("Basic C++ test application\n");
+  
+  for(i=0;i<10;i++) {
+    printf("Loop %d\n",i);
+  }
+  
+  return 0;
+}
+
---
+++ c++/QTCoreTest/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * File:   main.cpp
+ * Author: pmg
+ *
+ * Created on den 26 november 2012, 21:20
+ */
+
+#include <stdio.h>
+#include <QtGui/QApplication>
+
+
+
+int main(int argc, char *argv[]) {
+  printf("QTCore Test\n");
+  return 0;
+}
