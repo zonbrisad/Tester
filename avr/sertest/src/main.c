f74a8c33078890cee7749f4b75efd2b29ed39044
Peter Malmberg <peter.malmberg@gmail.com>
2023-05-25 18:15:12 +0200

    Added attribute command Added bar command

--- avr/sertest/src/main.c
+++ avr/sertest/src/main.c
@@ -20,31 +20,39 @@ void hw_init(void);
 
 void cmd_c16(char *args);
 void cmd_c256(char *args);
+void cmd_attr(char *args);
 void cmd_r100(char *args);
 void cmd_r1000(char *args);
 void cmd_r10000(char *args);
+void cmd_bar(char *args);
 void cmd_help(char *args);
 
-typedef enum {
+typedef enum
+{
 	EVENT_Timer1 = 0,
 } Events;
 
-static FILE mystdout = FDEV_SETUP_STREAM((void*)uart_putc, NULL, _FDEV_SETUP_WRITE);
+static FILE mystdout = FDEV_SETUP_STREAM((void *)uart_putc, NULL, _FDEV_SETUP_WRITE);
 
 LEF_Timer timer1;
 
 const PROGMEM LEF_CliCmd commands[] = {
-	LEF_CLI_LABEL("Color"),
-	{cmd_c16 ,   "c16",    "Print 16 terminal colors"},
-	{cmd_c256 ,  "c256",   "Print 256 terminal colors"},
+	LEF_CLI_LABEL("Text attributes"),
+	{cmd_c16, "c16", "Print 16 terminal colors"},
+	{cmd_c256, "c256", "Print 256 terminal colors"},
+	{cmd_attr, "attr", "Print attributes"},
 	LEF_CLI_LABEL("Generators"),
-	{cmd_r100,   "r100",   "Print 100 rows"},
-	{cmd_r1000,  "r1000",  "print 1000 rows"},
+	{cmd_r100, "r100", "Print 100 rows"},
+	{cmd_r1000, "r1000", "print 1000 rows"},
 	{cmd_r10000, "r10000", "print 10000 rows"},
-	{cmd_help,   "help",   "Print help information"},
+	LEF_CLI_LABEL("Animators"),
+	{cmd_bar, "bar", "Print progressbar"},
+	LEF_CLI_LABEL("Other"),
+	{cmd_help, "help", "Print help information"},
 };
 
-ISR(TIMER1_COMPA_vect) {
+ISR(TIMER1_COMPA_vect)
+{
 	TIMER1_RELOAD(0);
 	LEF_TimerUpdate(&timer1);
 }
@@ -52,135 +60,223 @@ ISR(TIMER1_COMPA_vect) {
 const PROGMEM char cc[][10] = {
 	E_BLACK, E_RED, E_GREEN, E_YELLOW, E_BLUE, E_MAGENTA,
 	E_CYAN, E_GRAY, E_DARKGRAY, E_BR_RED, E_BR_GREEN, E_BR_YELLOW,
-	E_BR_BLUE, E_BR_MAGENTA, E_BR_CYAN, E_WHITE
-};
+	E_BR_BLUE, E_BR_MAGENTA, E_BR_CYAN, E_WHITE};
+const PROGMEM char bgcc[][10] = {
+	E_BLACK, E_RED, E_GREEN, E_YELLOW, E_BLUE, E_MAGENTA,
+	E_CYAN, E_GRAY, E_DARKGRAY, E_BR_RED, E_BR_GREEN, E_BR_YELLOW,
+	E_BR_BLUE, E_BR_MAGENTA, E_BR_CYAN, E_WHITE};
 
-void cmd_c16(char *args) {
-	uint16_t i;
-  char buf[16];
-	UNUSED(args);
-	for (i=0; i<ARRAY_LENGTH(cc); i++) {
-		strcpy_P(buf, cc[i]);
-		printf("%s Color "E_END"\n", buf);
+void print_bar(int l, int max)
+{
+	int i;
+	char buf[96];
+	for (i = 0; i < l; i++)
+	{
+		buf[i] = '=';
 	}
+	buf[i] = '\0';
+	printf("  [%*s]  \n", max, buf);
+	//	printf("  [%-30s]  %2d\n", buf, l);
 }
 
-#define E_FG256(x) "\e[38;5;"#x"m"
-#define E_BG256(x) "\e[48;5;"#x"m"
-#define E_FG256P   "\e[38;5;%dm"
-#define E_BG256P   "\e[48;5;%dm"
+#define BAR 30
+void print_bar2(int l, int max)
+{
+	int i;
+	char buf[96];
+	for (i = 0; i < l; i++)
+	{
+		buf[i] = '=';
+	}
+	buf[l] = '\0';
+	//	printf("  [%*s]  %2d\n",30, buf, l);
+	printf("  [%-30s]  %2d", buf, l);
+}
 
-void cmd_c256(char *args) {
+void cmd_bar(char *args)
+{
+	int i;
+	UNUSED(args);
+	for (i = 0; i <= BAR; i++)
+	{
+		print_bar2(i, BAR);
+		_delay_ms(100);
+		//		printf(E_CPL);
+		printf("\r");
+	}
+	printf("\n");
+}
+
+#define E_FG256(x) "\e[38;5;" #x "m"
+#define E_BG256(x) "\e[48;5;" #x "m"
+#define E_FG256P "\e[38;5;%dm"
+#define E_BG256P "\e[48;5;%dm"
+
+void cmd_c16(char *args)
+{
+	int i;
+	UNUSED(args);
+
+	for (i = 0; i < 16; i++)
+	{
+		printf(E_FG256P " %3d " E_RESET, i, i);
+		if (i == 7)
+		{
+			printf("\n");
+		}
+	}
+	printf("\n\n");
+	for (i = 0; i < 16; i++)
+	{
+		printf(E_BG256P " %3d " E_RESET, i, i);
+		if (i == 7)
+		{
+			printf("\n");
+		}
+	}
+	printf("\n\n");
+}
+
+void cmd_c256(char *args)
+{
 	int i;
 	char buf[128];
 	char *p;
 	UNUSED(args);
 
-	p = buf;
-	printf("\n");
-	for (i=16; i<256; i++) {
+	cmd_c16("");
 
-		printf(E_FG256P" %3d "E_END,i,i);
-		p += sprintf(p, E_BG256P" %3d "E_END,i,i);
+	p = buf;
+
+	for (i = 16; i < 256; i++)
+	{
+
+		printf(E_FG256P " %3d " E_RESET, i, i);
+		p += sprintf(p, E_BG256P " %3d " E_RESET, i, i);
 		p++;
 		p[0] = ' ';
 		p--;
-		if (((i-15) % 6)==0) {
+		if (((i - 15) % 6) == 0)
+		{
 			p = buf;
-			printf("  %s\n",buf);
+			printf("  %s\n", buf);
 		}
 	}
 }
 
-void print_rows(int r) {
+void cmd_attr(char *args)
+{
+	printf("Text attributes\n");
+	printf("Normal text\n");
+	printf(E_BOLD "Bold text\n" E_RESET);
+	printf(E_DIM "Dim text\n" E_RESET);
+	printf(E_ITALIC "Italic text\n" E_RESET);
+	printf(E_UNDERLINE "Underline text\n" E_RESET);
+	printf(E_BLINK "Blink text\n" E_RESET);
+	printf(E_REVERSE "Reverse text\n" E_RESET);
+	printf(E_CROSSED "Crossed text\n" E_RESET);
+	printf(E_OVERLINED "Overlined text\n" E_RESET);
+}
+
+void print_rows(int r)
+{
 	int i;
-	for (i=1; i<=r; i++) {
-		printf("Row %d\n",i);
+	for (i = 1; i <= r; i++)
+	{
+		printf("Row %d\n", i);
 	}
 }
 
-void cmd_r100(char *args) {
+void cmd_r100(char *args)
+{
 	UNUSED(args);
 	print_rows(100);
 }
 
-void cmd_r1000(char *args) {
+void cmd_r1000(char *args)
+{
 	UNUSED(args);
 	print_rows(1000);
 }
 
-void cmd_r10000(char *args) {
+void cmd_r10000(char *args)
+{
 	UNUSED(args);
 	print_rows(10000);
 }
 
-void cmd_help(char *args) {
+void cmd_help(char *args)
+{
 	UNUSED(args);
 	LEF_Cli_print();
 }
 
-void printInfo(char *a, char *b) {
-	  defprintf(E_BR_MAGENTA"%-20s"E_END" "E_CYAN"%s\n"E_END, a, b);
+void printInfo(char *a, char *b)
+{
+	defprintf(E_BR_MAGENTA "%-20s" E_RESET " " E_CYAN "%s\n" E_RESET, a, b);
 }
 
-void printSysInfo(void) {
-	  char buf[16];
-	  printInfo("Build:",__DATE__"  "__TIME__);
-	  printInfo("C Standard:", STRINGIZE(__STDC_VERSION__));
+void printSysInfo(void)
+{
+	char buf[16];
+	printInfo("Build:", __DATE__ "  "__TIME__);
+	printInfo("C Standard:", STRINGIZE(__STDC_VERSION__));
 
-	#ifdef __GNUC__
-	  printInfo("GNU C ver:", __GNUC_VERSION__);
-	#endif
+#ifdef __GNUC__
+	printInfo("GNU C ver:", __GNUC_VERSION__);
+#endif
 
-	#ifdef __BIG_ENDIAN__
-	  printInfo("Byteorder:", "big endian");
-	#endif
-	#ifdef __LITTLE_ENDIAN__
-	  printInfo("Byteorder:", "little endian");
-	#endif
+#ifdef __BIG_ENDIAN__
+	printInfo("Byteorder:", "big endian");
+#endif
+#ifdef __LITTLE_ENDIAN__
+	printInfo("Byteorder:", "little endian");
+#endif
 
-	#ifdef __cplusplus
-	  printInfo("C++:", "enabled");
-	#endif
+#ifdef __cplusplus
+	printInfo("C++:", "enabled");
+#endif
 
-	#ifdef __OPTIMIZE__
-	#ifdef __OPTIMIZE_SIZE__
-	  printInfo("Optimization, size:", "Enabled");
-	#else
-	  printInfo("Optimization:", "Enabled");
-	#endif
-	#endif
+#ifdef __OPTIMIZE__
+#ifdef __OPTIMIZE_SIZE__
+	printInfo("Optimization, size:", "Enabled");
+#else
+	printInfo("Optimization:", "Enabled");
+#endif
+#endif
 
-	  sprintf(buf,"%d",sizeof(void*));
+	sprintf(buf, "%d", sizeof(void *));
 	//  printf("Size %d\n",sizeof(void*));
 
-	  printInfo("Pointer size:", buf);
+	printInfo("Pointer size:", buf);
 }
 
-void hw_init(void) {
-  stdout = &mystdout;
+void hw_init(void)
+{
+	stdout = &mystdout;
 
 	// Timer 1 (16 bit)
-	TIMER1_CLK_PRES_256();  // set prescaler to 1/1024
+	TIMER1_CLK_PRES_256(); // set prescaler to 1/1024
 	TIMER1_OCA_SET(625);
-	TIMER1_OCA_IE();        // enable output compare A interrupt
-	
-  uart_init(UART_BAUD_SELECT(UART_BAUD_RATE, F_CPU));
-  sei();  // Enable all interrupts
+	TIMER1_OCA_IE(); // enable output compare A interrupt
+
+	uart_init(UART_BAUD_SELECT(UART_BAUD_RATE, F_CPU));
+	sei(); // Enable all interrupts
 }
 
-int main() {
+int main()
+{
 	uint16_t ch;
 	LEF_Event event;
-	
+
 	LEF_Init();
 
 	LEF_TimerInit(&timer1, EVENT_Timer1);
 	LEF_TimerStartRepeat(&timer1, 10);
-	
-	//LEF_Button_init(&button, EVENT_Button);
-	//LEF_Buzzer_init();
-//	LEF_CliInit(commands, sizeof(commands) / sizeof((commands)[0]) );
+
+	// LEF_Button_init(&button, EVENT_Button);
+	// LEF_Buzzer_init();
+	//	LEF_CliInit(commands, sizeof(commands) / sizeof((commands)[0]) );
 	LEF_Cli_init(commands, LARRAY_LENGTH(commands));
 
 	hw_init();
@@ -188,21 +284,24 @@ int main() {
 	printf("\n\nArduino serial port testprogram\n\n");
 
 	printSysInfo();
-	
-	while (1) {
+
+	while (1)
+	{
 		LEF_Wait(&event);
-		
-		switch (event.id) {
-		 case EVENT_Timer1:
+
+		switch (event.id)
+		{
+		case EVENT_Timer1:
 			ch = uart_getc();
-			while ((ch & 0xff00) != UART_NO_DATA ) {
+			while ((ch & 0xff00) != UART_NO_DATA)
+			{
 				LEF_Cli_putc(ch);
 				ch = uart_getc();
 			}
-			
+
 			break;
-	 
-		 case LEF_EVENT_CLI:
+
+		case LEF_EVENT_CLI:
 			LEF_Cli_exec();
 			break;
 		}
--- c/motor/mmtest/src/mmmotor_settings.h
+++ c/motor/mmtest/src/mmmotor_settings.h
@@ -13,7 +13,8 @@
 #ifndef MMMOTOR_SETTINGS_H
 #define	MMMOTOR_SETTINGS_H
 
-typedef int16_t CHANNEL_VAL;
+//typedef int16_t CHANNEL_VAL;
+typedef int32_t CHANNEL_VAL;
 
 #define CHANNELS    8      // nr of channels
 #define ALARMS      10      // nr of alarms
