d5edc4e90da6f618f9349830a7d988308d402663
Peter Malmberg <peter.malmberg@gmail.com>
2024-07-31 14:34:35 +0200

    Added gdemo

---
+++ python/gdemo.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+#
+# gtk demo for python
+#
+# File:     gdemo
+# Author:   Peter Malmberg  <peter.malmberg@gmail.com>
+# Org:      __ORGANISTATION__
+# Date:     2024-07-30
+# License:
+# Python:   >= 3.0
+#
+# ----------------------------------------------------------------------------
+
+
+import traceback
+import os
+import sys
+import logging
+import argparse
+
+import gi
+
+gi.reqgi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+
+class App:
+    NAME = "gdemo"
+    VERSION = "0.01"
+    DESCRIPTION = "gtk demo for python"
+    LICENSE = ""
+    AUTHOR = "Peter Malmberg"
+    EMAIL = "<peter.malmberg@gmail.com>"
+    ORG = "__ORGANISATION__"
+    HOME = ""
+    ICON = ""
+
+
+def main() -> None:
+    logging_format = "[%(levelname)s] %(lineno)-4d %(funcName)-14s : %(message)s"
+    #    logging.basicConfig(format=logging_format, level=logging.DEBUG)
+
+    parser = argparse.ArgumentParser(
+        prog=App.NAME, description=App.DESCRIPTION, epilog="", add_help=True
+    )
+    parser.add_argument(
+        "--version",
+        action="version",
+        version=f"{App.NAME} {App.VERSION}",
+        help="Print version information",
+    )
+
+    parser.add_argument(
+        "--debug", action="store_true", default=False, help="Print debug messages"
+    )
+
+    args = parser.parse_args()
+
+    if args.debug:
+        logging.basicConfig(format=logging_format, level=logging.DEBUG)
+
+    win = Gtk.Window()
+    win.connect("destroy", Gtk.main_quit)
+    win.show_all()
+    Gtk.main()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+        sys.exit(0)
+    except KeyboardInterrupt as e:  # Ctrl-C
+        raise e
+    except SystemExit as e:  # sys.exit()
+        raise e
+    except Exception as e:
+        print("ERROR, UNEXPECTED EXCEPTION")
+        print(str(e))
+        traceback.print_exc()
+        os._exit(1)
